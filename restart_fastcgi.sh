fec360942f65c18100225a68de448b4610853d55
root <root@localhost.localdomain>
2010-04-23 20:20:33 +0800

    upload

---
+++ restart_fastcgi.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+echo "Resting......"
+
+PROJDIR="/zzdata/gamecard"
+PIDFILE="/tmp/gamecard.pid"
+SOCKET="/tmp/gamecard.sock"
+
+cd $PROJDIR
+if [ -f $PIDFILE ]; then
+    kill `cat -- $PIDFILE`
+    rm -f -- $PIDFILE
+fi
+
+/usr/bin/python $PROJDIR/manage.py runfcgi method=prefork socket=$SOCKET pidfile=$PIDFILE
+/bin/chown nobody $SOCKET
