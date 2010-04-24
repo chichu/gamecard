de17fc4d386bf508498667fa71fd2476130e3d77
chichu <apple@localhost.(none)>
2010-04-24 20:16:38 +0800

    edit pages

--- card/admin.py
+++ card/admin.py
@@ -7,7 +7,7 @@ from gamecard.utils.strutils import check_cardfile_format
 
 class GamesAdmin(admin.ModelAdmin):
     list_display = ('name','game_type','create_time')
-    search_fields = list_display
+    search_fields = ('name',)
     radio_fields = {"game_type": admin.HORIZONTAL}
     list_filter = ('game_type',)
     readonly_fields = ('create_time',)
@@ -15,26 +15,26 @@ admin.site.register(Games,GamesAdmin)
 
 class ItemAdmin(admin.ModelAdmin):
     list_display = ('name','format','is_chance','chance_time_delta','max_apply_perday')
-    search_fields = list_display
+    search_fields = ('name',)
     radio_fields = {"format":admin.HORIZONTAL}
     list_filter = ('format','is_chance')
 admin.site.register(Item,ItemAdmin)
 
 class ActAdmin(admin.ModelAdmin):
     list_display = ('name','item','game','card_count','card_type','start_time','status')
-    search_fields = list_display
+    search_fields = ('name',)
     list_filter = ('status','card_type')
     radio_fields = {"status": admin.HORIZONTAL,"card_type":admin.HORIZONTAL}
 admin.site.register(Activity,ActAdmin)
 
 class NoticeAdmin(admin.ModelAdmin):
     list_display = ('title','content','activity','create_time')
-    search_fields = list_display
+    search_fields = ('title',)
 admin.site.register(Notice,NoticeAdmin)
 
 class AnounceAdmin(admin.ModelAdmin):
     list_display = ('content','create_time')
-    search_fields = list_display
+    search_fields = ('content',)
 admin.site.register(Anounce,AnounceAdmin)
    
 class FileLoaderAdmin(admin.ModelAdmin):
