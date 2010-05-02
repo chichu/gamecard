c12848ca947b7a88aec7a8df8642feb1758f92cd
chichu <apple@localhost.(none)>
2010-05-02 11:03:12 +0800

    to be tuned

--- static/js/jShowWin.js
+++ static/js/jShowWin.js
@@ -3,7 +3,8 @@ $(function(){
 	var winClose=$(".WinClose");
 	var showWin=$("#ShowWin");
 	var docHeight=$(document).height();
-	showWin.click(function(){$(".pop-up-win").fadeIn("fast");$(".pop-wrap").show().height(docHeight);});
+	//showWin.click(function(){$(".pop-up-win").fadeIn("fast");$(".pop-wrap").show().height(docHeight);});
+	$(".pop-up-win").fadeIn("fast");$(".pop-wrap").show().height(docHeight);
 	winClose.click(function(){$(".pop-up-win").fadeOut("fast");$(".pop-wrap").hide("");});
 
 })
