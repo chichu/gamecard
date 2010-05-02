1111fa444e63f8f5337460b25923f263af19d772
root <root@localhost.localdomain>
2010-05-02 16:50:45 +0800

    aa

--- static/js/jShowWin.js
+++ static/js/jShowWin.js
@@ -1,10 +1,13 @@
 // JavaScript Document
 $(function(){
 	var winClose=$(".WinClose");
-	//var showWin=$("#ShowWin");
 	var docHeight=$(document).height();
-	//showWin.click(function(){$(".pop-up-win").fadeIn("fast");$(".pop-wrap").show().height(docHeight);});
-	$(".pop-up-win").fadeIn("fast");$(".pop-wrap").show().height(docHeight);
-	winClose.click(function(){$(".pop-up-win").fadeOut("fast");$(".pop-wrap").hide();$(".pop-wrap").detach();});
+	$(".pop-up-win").fadeIn("fast");
+	//$(".pop-up-win").show();
+	$(".pop-wrap").show().height(docHeight);
+	winClose.click(function(){
+		$(".pop-up-win").remove();
+		$(".pop-wrap").remove();
+	});
 
 })
