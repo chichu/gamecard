618df91ea2bca2b0de4b9661d2dc53b0565d6f2f
root <root@localhost.localdomain>
2010-05-04 18:43:14 +0800

    finish in popup

--- static/js/jShowWin.js
+++ static/js/jShowWin.js
@@ -1,30 +1,28 @@
-// JavaScript Document
 function get_popup(url){
-    $.get(url, function(data) {
-        $("#pop-pu").append(data);
-    }); 
+$.get(url, function(data) {
+    $("#pop-up").append(data);
       
-	var docHeight=$(document).height();
-	//$(".pop-up-win").fadeIn("fast");
-	//$(".pop-wrap").show().height(docHeight);
-	$(".pop-wrap").height(docHeight);
+    var docHeight=$(document).height();
+    $(".pop-up-win").fadeIn("fast");
+    $(".pop-wrap").show().height(docHeight);
+    $(".pop-wrap").height(docHeight);
 	
     var divWidth=$(".pop-up-win").width()/2;
     var divHeight=$(".pop-up-win").height()/2;
     var divLeft =  document.documentElement.clientWidth/2 - divWidth;
-    var divTop =  document.documentElement.clientHeight/2 - divHeight/2;
+    var divTop =  document.documentElement.clientHeight/2 - divHeight/2 -100;
     $(".pop-up-win").css({"left":divLeft,"top":divTop});
     
     $('.WinClose').click(function(){
-        $(".pop-up-win").hide();
-    	$(".pop-wrap").hide();
+       $(".pop-up-win").remove();
+       $(".pop-wrap").hide();
     });
     
     $(window).scroll(function(){
-        var divScrollTop = document.documentElement.scrollTop + divTop -40;
+        var divScrollTop = document.documentElement.scrollTop + divTop;
         var divScrollLeft = divLeft - document.documentElement.scrollLeft/2;
         $(".pop-up-win").animate({ top: divScrollTop + "px",left: divScrollLeft + "px",},-100); 
     });
     
-    
+}); 
 }
