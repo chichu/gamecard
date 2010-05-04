7fd3fdb36534027cc9f9d419d4d75973ab9bd26d
chichu <apple@localhost.(none)>
2010-05-04 15:51:00 +0800

    modify css

--- static/js/jShowWin.js
+++ static/js/jShowWin.js
@@ -1,25 +1,29 @@
 // JavaScript Document
-$(function(){
-	var winClose=$(".WinClose");
+function get_popup(url){
+    $.get(url, function(data) {
+        $("#pop-up").append(data);
+    }); 
+      
 	var docHeight=$(document).height();
 	$(".pop-up-win").fadeIn("fast");
-	//$(".pop-up-win").show();
 	$(".pop-wrap").show().height(docHeight);
-	winClose.click(function(){
-		$(".pop-up-win").remove();
-		$(".pop-wrap").remove();
-	});
 	
     var divWidth=$(".pop-up-win").width()/2;
     var divHeight=$(".pop-up-win").height()/2;
     var divLeft =  document.documentElement.clientWidth/2 - divWidth;
     var divTop =  document.documentElement.clientHeight/2 - divHeight -120;
-    var divScrollTop = document.documentElement.scrollTop + divTop;
-    var divScrollLeft = divLeft - document.documentElement.scrollLeft/2; 
     $(".pop-up-win").css({"left":divLeft,"top":divTop});
-    //$(".pop-up-win").animate({ top: divScrollTop + "px",left: divScrollLeft + "px",},1); 
+    
     $(window).scroll(function(){
+        var divScrollTop = document.documentElement.scrollTop + divTop;
+        var divScrollLeft = divLeft - document.documentElement.scrollLeft/2;
         $(".pop-up-win").animate({ top: divScrollTop + "px",left: divScrollLeft + "px",},1); 
     });
+    
+    var winClose=$(".WinClose");
+    winClose.click(function(){
+		$(".pop-up-win").remove();
+		$(".pop-wrap").remove();
+	});
 
 })
