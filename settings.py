554ea45421af46583614da974c1bd8423837b7e5
root <root@localhost.localdomain>
2010-05-24 12:18:27 +0800

    test

--- settings.py
+++ settings.py
@@ -100,9 +100,9 @@ SESSION_ENGINE = 'django.contrib.sessions.backends.cache'
 #SESSION_COOKIE_SECURE = True
 
 TEMPLATE_CONTEXT_PROCESSORS = (
-    "django.core.context_processors.request"
+    "django.core.context_processors.request",
     #"django.contrib.auth.context_processors.auth",
-    "django.contrib.context_processors.auth",
+    "django.core.context_processors.auth",
     "django.core.context_processors.debug",
     "django.core.context_processors.i18n",
     "django.core.context_processors.media",
