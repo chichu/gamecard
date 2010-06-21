4eff767c2dc12042877d7cd03779886be20e7ee9
root <root@localhost.localdomain>
2010-06-21 18:11:45 +0800

    test

--- middlewares.py
+++ middlewares.py
@@ -1,6 +1,13 @@
 # encoding: utf-8
+from gamecard.utils.strutils import get_username_from_cookie
+from gamecard.log import log_error
+
 class AuthMiddleware(object):
     def process_request(self, request):
-        if not request.session.has_key('username'):
+        try:
+            #if not request.session.has_key('username'):
             request.session['username'] = get_username_from_cookie(request)
+        except Exception,e:
+            log_error("error in Authmiddleware:%s request sesion:%s"%(e,request.session['username']))
+            return None
         return None
